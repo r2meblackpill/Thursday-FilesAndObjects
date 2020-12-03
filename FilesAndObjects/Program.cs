dcdeb097433db60388b83c49e315d144a9ca41b7
Opilane TTHK <opilane@tthk.ee>
2020-12-03 12:52:15 +0200

    first

---
+++ FilesAndObjects/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FilesAndObjects
+{
+    class Program
+    {
+        class Movie
+        {
+            public string title;
+            public string rating;
+            public string year;
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\julia.voronetskaja\samples";
+            string fileName = @"imdb.txt";
+            //a list to store the movies from the file
+            List<string> movieList = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+            List<Movie> listOfMovies = new List<Movie>();
+
+
+            foreach(string movieItem in movieList)
+            {
+                string[] tempArray = movieItem.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[2]);
+
+                listOfMovies.Add(newMovie);
+            }
+
+            foreach(Movie movie in listOfMovies)
+            {
+                Console.WriteLine($"Title: {movie.title}; Rating: {movie.rating}; Year: {movie.year}");
+               
+            }
+
+            Console.WriteLine("What is your favorite movie? enter the title:");
+            string favMovieTitle = Console.ReadLine();
+            Console.WriteLine("Enter your favorite movie rating:");
+            string favMovieRating = Console.ReadLine();
+            Console.WriteLine("Enter the release year:");
+            string favMovieYear = Console.ReadLine();
+
+            Movie favMovie = new Movie(favMovieTitle, favMovieRating, favMovieYear);
+            string favMovieLine = $"{favMovie.title};{favMovie.rating};{favMovie.year}";
+
+            movieList.Add(favMovieLine);
+            File.WriteAllLines(Path.Combine(filePath, fileName), movieList);
+        }
+    }
+}
---
+++ ReadTotalFromShoppingList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadTotalFromShoppingList
+{
+    class Program
+    {
+        public class Item
+        {
+            string name;
+            int price;
+
+            public Item(string _name, int _price)
+            {
+                name = _name;
+                price = _price;
+                Console.WriteLine($"DEBUG: Item {name} created.");
+            }
+
+            public string Name { get { return name; } }
+            public int Price { get { return price; } }
+        }
+        static void Main(string[] args)
+        {
+            ReadFromShoppingList();
+        }
+
+        public static void ReadFromShoppingList()
+        {
+            string filePath = @"C:\Users\opilane\samples";
+            string fileName = @"shoppingList.txt";
+            
+            List<Item> shoppingItems = new List<Item>();
+
+            List<string> linesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+
+            foreach(string line in linesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                Item newItem = new Item(tempArray[0], Int32.Parse(tempArray[1]));
+                shoppingItems.Add(newItem);
+                
+                //Console.WriteLine($"Item: {tempArray[0]}");
+                //Console.WriteLine($"Price: {tempArray[1]}");
+            }
+            Console.WriteLine("Your shopping cart contains:");
+            int total = 0;
+
+
+            foreach(Item item in shoppingItems)
+            {
+                Console.WriteLine($"Item: {item.Name} | Price: {item.Price}");
+                total += item.Price;
+            }
+
+            Console.WriteLine("Shopping cart total:" + total);
+        }
+    }
+}
